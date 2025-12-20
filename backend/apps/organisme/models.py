5ff7c96f5a37adb8dfa46bfc64a635111a03ed32
Jean-Baptiste Baillet <jbbaillet.dev@gmail.com>
2025-12-20 13:27:05 +0000

    feat: ajout de l'app organisme

--- backend/apps/organisme/models.py
+++ backend/apps/organisme/models.py
@@ -1,39 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-
-class Reseau(models.Model):
-    nom: models.CharField = models.CharField(
-        max_length=150,
-        unique=True,
-        verbose_name=_("Nom"),
-        help_text=_(
-            "Exemples : Santé au travail, Handicap, Sécurité sociale, Emploi Formation"
-        ),
-    )
-
-    description: models.TextField = models.TextField(
-        verbose_name=_("Description"),
-        blank=True,
-    )
-
-    date_creation: models.DateTimeField = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name=_("Créé le"),
-    )
-
-    date_modification: models.DateTimeField = models.DateTimeField(
-        auto_now=True,
-        verbose_name=_("Modifié le"),
-    )
-
-    class Meta:
-        verbose_name = _("Réseau")
-        verbose_name_plural = _("Réseaux")
-        ordering = ["nom"]
-
-    def __str__(self) -> str:
-        return self.nom
+from apps.reseaux.models import Reseau
 
 
 class Adresse(models.Model):
